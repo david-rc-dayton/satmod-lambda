79ec5d3c16ce19056008c61e3b40afbbf5b6b89d
David RC Dayton <david.rc.dayton@gmail.com>
2014-09-18 19:55:25 -0600

    implemented remove-construct functionality

--- src/satmod_lambda/ui/edit.clj
+++ src/satmod_lambda/ui/edit.clj
@@ -18,11 +18,28 @@
                      (let [t (.trim (s/text name-field))
                            category (get data/categories 
                                          (.getSelectedItem combobox))]
-                               (when-not (.isEmpty t)
-                                 (data/add-construct! category t))))]
+                               (if-not (.isEmpty t)
+                                 (data/add-construct! category t)
+                                 (s/alert "Name cannot be blank."))
+                               (category-fn listbox combobox)))]
     (-> (s/dialog :content (s/grid-panel :items [construct-str name-field])
-              :option-type :ok-cancel :success-fn (partial success-fn))
+                  :option-type :ok-cancel :success-fn (partial success-fn))
       s/pack! s/show!)))
+
+(defn remove-fn
+  "Generate dialog to remove construct"
+  [listbox combobox & _]
+  (let [selected-type (get data/categories (.getSelectedItem combobox))
+        selected-name (:name (.getSelectedValue listbox))
+        selected-id (:id (.getSelectedValue listbox))
+        rm-str (str "Are you sure you want to remove " selected-name "?")
+        success-fn (fn [& _] (data/remove-construct! 
+                               selected-type selected-id)
+                     (category-fn listbox combobox))]
+    (when-not (nil? selected-name)
+      (-> (s/dialog :content (s/grid-panel :items [rm-str])
+                    :option-type :yes-no :success-fn (partial success-fn))
+        s/pack! s/show!))))
 
 (defn select-panel
   "Generate selection panel."
@@ -36,6 +53,9 @@
                                                selection-box category-box))
     (s/listen add-button :action (partial add-fn
                                           selection-box category-box))
+    (s/listen remove-button :action (partial remove-fn
+                                             selection-box category-box))
+    (category-fn selection-box category-box)
     (s/border-panel :north category-box :center (s/scrollable selection-box) 
                     :south button-panel :border data/border-size)))
 

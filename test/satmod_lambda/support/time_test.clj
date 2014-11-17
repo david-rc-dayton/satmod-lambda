da246205af99f7b1674ef8d25dcd29bab4dcf5b3
David RC Dayton <david.rc.dayton@gmail.com>
2014-11-17 12:07:31 -0700

    added unit tests for support.time

---
+++ test/satmod_lambda/support/time_test.clj
@@ -0,0 +1,10 @@
+(ns satmod-lambda.support.time-test
+  (:require [clojure.test :refer :all]
+            [satmod-lambda.support.time :refer :all]))
+
+(deftest date-test
+  (testing "date conversion"
+           (let [date-map {:year 2014 :month 11 :day 17
+                           :hour 19 :minute 04 :second 48}]
+             (is (= (type (now)) java.util.Date))
+             (is (= (date->hash-map (hash-map->date date-map)) date-map)))))

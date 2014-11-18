3af43969e4799ee86fc59d2c4e7b91a98674e202
David RC Dayton <david.rc.dayton@gmail.com>
2014-11-18 14:18:05 -0700

    added options-panel in coverage window

--- src/satmod_lambda/ui/coverage.clj
+++ src/satmod_lambda/ui/coverage.clj
@@ -1,6 +1,7 @@
 (ns satmod-lambda.ui.coverage
   (:require [satmod-lambda.support.time :as time]
-            [seesaw.core :as s])
+            [seesaw.core :as s]
+            [seesaw.mig :as sm])
   (:import [java.awt.event ActionListener]))
 
 (def root (atom nil))
@@ -13,7 +14,7 @@
   [slider label & _]
   (let [hour (int (/ (s/selection slider) 60))
         minute (rem (s/selection slider) 60)
-        text (format "%02d:%02dz" hour minute)]
+        text (format "  %02d:%02dz  " hour minute)]
     (s/text! label text)
     (when-not (.getValueIsAdjusting slider)
       (swap! simulation-time merge {:hour hour :minute minute}))))
@@ -34,17 +35,35 @@
                               :min 0 :max 1439 :value 0 
                               :minor-tick-spacing 60 :major-tick-spacing 240
                               :paint-track? true)
-        time-label (s/label :id :time-label :text "00:00z")
+        time-label (s/label :id :time-label :text "  00:00z  ")
         date-picker (doto (org.jdesktop.swingx.JXDatePicker.)
                       (.setTimeZone (java.util.TimeZone/getTimeZone "UTC")))]
     (.setDate date-picker (time/now))
     (s/listen time-slider :selection (partial time-fn time-slider time-label))
     (.addActionListener date-picker
       (reify ActionListener (actionPerformed [& _] (date-fn date-picker))))
-    (s/horizontal-panel :items [time-label time-slider date-picker])))
+    (s/horizontal-panel :items [date-picker time-label time-slider])))
+
+(defn option-panel
+  "Generate options panel in coverage window"
+  []
+  (let [image-button (s/button :id :image-button :text "Save Image")
+        animate-button (s/button :id :animate-button :text "Save Animation")
+        grid-toggle (s/toggle :id :grid-toggle :text "Show Grid Lines")]
+    (s/scrollable (sm/mig-panel :items [[image-button "span, grow"] 
+                                        [animate-button "span, grow"]
+                                        [(s/separator) "span, grow"]
+                                        [grid-toggle "span, grow"]])
+                  :hscroll :never)))
+
+(defn map-panel
+  "Generate coverage map panel in coverage window."
+  []
+  (s/horizontal-panel :items ["(insert map here)"]))
 
 (defn coverage-panel
   "Panel for viewing satellite coverage."
   []
   (reset! root (s/border-panel :north (time-panel)
-                               :center "coverage-panel")))
+                               :west (option-panel)
+                               :center (map-panel))))
